cd4a5d6c2e3c19da671a22e96032f074c4df4f65
MatthewF <153161493+MattFA24@users.noreply.github.com>
2025-06-01 17:38:50 +0700

    add

---
+++ ALP_Jevon_Carry/view/SessionHistoryView.swift
@@ -0,0 +1,18 @@
+//
+//  SessionHistoryView.swift
+//  ALP_Jevon_Carry
+//
+//  Created by Daffa Khoirul on 01/06/25.
+//
+
+import SwiftUI
+
+struct SessionHistoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SessionHistoryView()
+}
